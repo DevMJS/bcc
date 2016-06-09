f4ed9aec3826969003aee0b4252483d664861146
MarcosJS <marcos.com.j.s@gmail.com>
2016-06-09 18:20:39 +0000

    Replaced files

--- aed/projeto/main.c
+++ aed/projeto/main.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-<<<<<<< HEAD
-<<<<<<< HEAD
 //#include "functionsforinterfaces.h"
 #include "menugmed.h"
 
@@ -12,25 +10,6 @@ int main(int argc, char *argv[]){
 	system("clear");
 
 	while(mod != 0){
-=======
-#include <string.h>
-#include "estruturaseconstantes.h"
-#include "funcoes.h"
-#include "funcoesdeinterfaces.h"
-
-=======
-#include <string.h>
-#include "estruturaseconstantes.h"
-#include "funcoes.h"
-#include "funcoesdeinterfaces.h"
-
->>>>>>> bca1297a05905ddcbbadcbfdd9e4875efbc53431
-int main(int argc, char* argv[]) {
-	int opcao = 1;
-	listafunc matriz;
-	funcionario elemento;
-	int posicao;
->>>>>>> origin/master
 	
      
 		switch(mod) {
@@ -38,15 +17,7 @@ int main(int argc, char* argv[]) {
 				leave(&mod);
 				break;
 			case 1:
-<<<<<<< HEAD
-<<<<<<< HEAD
 				mod = showMenu("textforinterfaces/menumain.txt");
-=======
-				opcao = exibirMenu();
->>>>>>> origin/master
-=======
-				opcao = exibirMenu();
->>>>>>> bca1297a05905ddcbbadcbfdd9e4875efbc53431
 				break;
 				    
 			case 2:
@@ -59,121 +30,8 @@ int main(int argc, char* argv[]) {
 				break;
 
 			case 4:
-<<<<<<< HEAD
 				ShowMsgAlert("textforinterfaces/alertconstruct.txt");
 				mod = 1;
-=======
-				system("clear");
-				if(cheiaFunc(&matriz)) {
-					printf("\n\n\t|                         Lista cheia!                        |");
-				} else {
-					printf("\n\n\t|                   A lista nao esta cheia!                   |");
-				}
-					opcao = 1;
-				getchar();
-				break;
-			case 5:
-				system("clear");
-				printf("\n\n\t|                         Tamanho = %d                        |", tamanhoFunc(&matriz));
-				opcao = 1;
-				getchar();
-				break;
-			case 6:
-				system("clear");
-				printf("\n\n\t|     Selecione a posicao do elemento para carregar: ");
-				scanf("%d", &posicao);
-				getchar();
-				if(carregarFunc(&matriz, posicao, &elemento)) {
-					printf("\n\n\t|                  O elemento carregado e: %s.                |", elemento.nome);
-				} else {
-					printf("\n\n\t|                     Posicao invalida!                       |");
-					printf("\n\t|   A lista esta vazia ou nao entrou com valores de 1 a 50.   |");
-					printf("\n\t|    Confira o tamanho da lista antes de tentar esta opcao!   |");
-				}
-				opcao = 1;
-				getchar();
-				break;
-			case 7:
-				system("clear");
-				printf("\n\n\t|     Informe o elemento que sera inserido no fim da lista: ");
-				fgets(elemento.cpf, 12, stdin);
-				limparBuffer();
-				//getchar();
-				if(inserirFimFunc(&matriz, elemento)) {
-					printf("\n\n\t|                     Elemento inserido!                      |");
-				} else {
-					printf("\n\n\t|                        Lista cheia!                         |");
-				}
-				opcao = 1;
-				getchar();
-				break;
-			case 8:
-				system("clear");
-				printf("\n\n\t|     Informe o elemento a ser inserido: ");
-				fgets(elemento.cpf, 12, stdin);
-				limparBuffer();
-				//getchar();
-				printf("\t|     Informe a posicao em que o elemento sera inserido: ");
-				scanf("%d", &posicao);
-				getchar();
-				if(posicao == 1 && tamanhoFunc(&matriz) == 0) {
-					inserirFimFunc(&matriz, elemento);
-					printf("\n\n\t|                     Elemento inserido!                      |");
-				} else {
-					if(inserirPosicaoFunc(&matriz, elemento, posicao)) {
-						printf("\n\n\t|                     Elemento inserido!                      |");
-					} else {
-						printf("\n\n\n\n\n\n\t|  Lista cheia ou nao esta sendo incrementada sequecialmente! |");
-						printf("\n\t|    Confira o tamanho da lista antes de tentar esta opcao!   |");
-					}
-				}
-				opcao = 1;
-				getchar();
-				break;
-			case 9:
-				system("clear");
-				if(removerFimFunc(&matriz)) {
-					printf("\n\n\t|                     Elemento removido!                      |");
-				} else {
-					printf("\n\n\t|                     A lista esta vazia!                     |");
-				}
-				opcao = 1;
-				getchar();
-				break;
-			case 10:
-				system("clear");
-				printf("\n\n\t|     Informe a posicao do elemento a ser removido: ");
-				scanf("%d", &posicao);
-				getchar();
-				if(removerPosicaoFunc(&matriz, posicao)) {
-					printf("\n\n\t|                     Elemento removido!                      |");
-				} else {
-					printf("\n\n\t|Lista esta vazia ou tentou remover de posicoes inexistentes! |");
-					printf("\n\t|    Confira o tamanho da lista antes de tentar esta opcao!   |");
-				}
-				opcao = 1;
-				getchar();
-				break;
-			case 11:
-				system("clear");
-				printf("\n\n\t|     Informe o elemento a ser encontrado: ");
-				fgets(elemento.cpf, 12, stdin);
-				limparBuffer();
-				//getchar();
-				if(procurarElementoFunc(&matriz, elemento) == -1) {
-					printf("\n\n\t|                   Elemento nao encontrado!                  |");
-				} else {
-					printf("\n\n\t|               O elemento esta na posicao:%d            |", procurarElementoFunc(&matriz, elemento));
-				}
-				opcao = 1;
-				getchar();
-				break;
-			case 12:
-				system("clear");
-				exibirElementosFunc(&matriz);
-				opcao = 1;
-				getchar();
->>>>>>> origin/master
 				break;
 
 			default:
